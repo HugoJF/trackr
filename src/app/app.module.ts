d039f96043805b992aafa9ec78028d813f885b36
Hugo Jeller <hugo_jeller@hotmail.com>
2021-06-30 11:46:58 -0400

    Trigger build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {HeaderComponent} from './header/header.component';
 import {SharedModule} from "./shared/shared.module";
