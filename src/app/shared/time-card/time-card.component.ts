f5a0c699daeb741c9cbf8b7137e3d0d42a5ffba5
Hugo Jeller <hugo_jeller@hotmail.com>
2021-06-30 16:21:01 -0400

    Fixes time card not converting timezones

--- src/app/shared/time-card/time-card.component.ts
+++ src/app/shared/time-card/time-card.component.ts
@@ -1,10 +1,22 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {TimeCard} from "../../../types";
+import {zonedTimeToUtc} from "date-fns-tz";
 
 @Component({
   selector: 'app-time-card[period]',
   templateUrl: './time-card.component.html'
 })
-export class TimeCardComponent {
+export class TimeCardComponent implements OnInit {
   @Input() period!: TimeCard[];
+
+  from!: Date;
+  to?: Date;
+
+  ngOnInit(): void {
+    this.from = zonedTimeToUtc(`${this.period[0].date}T${this.period[0].time}:00`, 'America/Sao_Paulo');
+    if (this.period[1]) {
+      this.to = zonedTimeToUtc(`${this.period[1].date}T${this.period[1].time}:00`, 'America/Sao_Paulo');
+    }
+  }
+
 }
